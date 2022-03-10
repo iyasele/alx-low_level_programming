085d2a4fdf11ea7fcf1fdfdceca72cfcff8bc1a7
iyasele <mailiykomo@gmail.com>
2022-03-10 21:39:15 +0000

    added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
